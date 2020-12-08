05992601b39b52562f899e737782b33ae187d904
tobik11 <tobik11@onet.pl>
2020-12-08 12:40:02 +0000

    tr

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -14,7 +14,7 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector<char> mons;
     for (auto it = people.rbegin(); it != people.rend(); it++) {
         // std::cout << *it.getAge() << std::endl;
-        if(*it->isMonster())
+        if(it->isMonster())
             mons.push_back('n');
         else
             mons.push_back('y');
